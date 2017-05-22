1330828353946f5b82d042ae694459e65781621f
杨璐 <812708519@qq.com>
2017-05-22 11:44:09 +0800

    修改报错方法， 系统错误会返回对应的json

--- src/main.go
+++ src/main.go
@@ -9,8 +9,6 @@ import (
 	"github.com/astaxie/beego/logs"
 	"runtime"
 	"common"
-	"strconv"
-
 	"models"
 )
 
@@ -21,7 +19,9 @@ func main() {
 	}
 
 	// 修改了默认的panic处理方法， 原方法在 config.go
-	// 保证原有处理逻辑不变， 仅增加了处理serviceException的方法
+	// panic之后
+	// 如果是serviceException： 返回json信息
+	// 否则 输出 未知的系统错误, 并打印栈，
 	beego.BConfig.RecoverFunc = func(ctx *context.Context) {
 		if err := recover(); err != nil {
 
@@ -31,46 +31,31 @@ func main() {
 			if !beego.BConfig.RecoverPanic {
 				panic(err)
 			}
+
 			// 新加逻辑， 如果可以转换为serviceException
 			se, cok := err.(common.ServiceException)
 			if cok {
 				ctx.Output.SetStatus(400)
 				ctx.Output.Body(se.Json())
 				return
-			}
-			serviceError, cok := err.(common.ServiceError)
-			if cok {
+			} else {
 				ctx.Output.SetStatus(500)
-				ctx.Output.Body(serviceError.Json())
+				ctx.Output.Body(common.NewServiceError(10001).Json())
+
+				var stack string
+				logs.Critical("the request url is ", ctx.Input.URL())
+				logs.Critical("Handler crashed with error", err)
+				for i := 1; ; i++ {
+					_, file, line, ok := runtime.Caller(i)
+					if !ok {
+						break
+					}
+					logs.Critical(fmt.Sprintf("%s:%d", file, line))
+					stack = stack + fmt.Sprintln(fmt.Sprintf("%s:%d", file, line))
+				}
 				return
 			}
-			// 修改 私有方法调用为 公共方法   转换数据
-			if beego.BConfig.EnableErrorsShow {
-				if _, ok := beego.ErrorMaps[fmt.Sprint(err)]; ok {
-					ecode, e := strconv.ParseInt(fmt.Sprint(err), 10, 64)
-					if e != nil {
-						panic("error code parse error" + e.Error())
-					}
-					beego.Exception(uint64(ecode), ctx)
-					return
-				}
-			}
 
-			var stack string
-			logs.Critical("the request url is ", ctx.Input.URL())
-			logs.Critical("Handler crashed with error", err)
-			for i := 1; ; i++ {
-				_, file, line, ok := runtime.Caller(i)
-				if !ok {
-					break
-				}
-				logs.Critical(fmt.Sprintf("%s:%d", file, line))
-				stack = stack + fmt.Sprintln(fmt.Sprintf("%s:%d", file, line))
-			}
-			if beego.BConfig.EnableErrorsRender {
-				//showErr(err, ctx, stack)
-
-			}
 		}
 	}
 
